5d6fecbed21049e85c6fa9dabbbcd2d39bf9ae21
Ahmed Nagy <zamalekahmed196@gmail.com>
2025-12-02 22:16:32 +0200

    Add files via upload

---
+++ PHP/task1_to_5.php
@@ -0,0 +1,37 @@
+<?php //التكليف 01
+/*My Name Is  Ahmed_Nage  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=" My First PHP Page"?></title>
+  </head>
+  <body>
+    <div><?php echo "We Love"?></div>
+    <div><?php echo"Elzero Channel"?></div>
+  </body>
+</html>
+<?php 
+//2------------------
+//echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+#echo 'Prevent Me From Running Please';
+
+?>
+<?php 
+//3--------------------
+/*
+ My Application
+ Version 1.0
+ Created By Elzero
+ */
+?>
+<?php
+//4-------
+// ## First Comment      سليم
+# // # Second Comment   سليم
+/* /* /* Third Comment */ //خطا
+////// Fourth Comment  سليم
---
+++ PHP/task6-12.php
@@ -0,0 +1,25 @@
+<?php 
+//1-----------
+echo (int)(15.2 + 14.7 + (10.5 + 10.5)); // 50
+echo gettype( (int)(15.2 + 14.7 + (10.5 + 10.5))); // Integer
+//2-----------------------------------
+echo'<br>';
+echo gettype(100);
+echo '<br>';
+var_dump (100);
+echo '<br>';
+echo is_int(100);
+//3-------------------------
+echo '<br>';
+echo "Hello \"Elzero\" \\\\ \"\"\" We Love \"\$\$PHP\"";
+//4------------------------------
+echo '<br>';
+echo nl2br("We \n Love \n Elzero \n Web \n School");
+echo '<br>';
+//5-----------------------------
+echo "Hello \"'Elzero'\"";
+echo '<br>';
+echo"We Love \$Programming\$";
+echo '<br>';
+echo 'Languages Specially "PHP"'
+?>
